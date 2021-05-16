1a6e57b02237c8312cc41cb276f4e9c16e7b0bb5
Starlet <lstar2397@naver.com>
2021-05-16 15:31:12 +0900

    Initialize project using Spring Initializr

---
+++ src/main/java/kr/fabua/pdfcon/PdfconApplication.java
@@ -0,0 +1,13 @@
+package kr.fabua.pdfcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfconApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfconApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/fabua/pdfcon/PdfconApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.fabua.pdfcon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfconApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
